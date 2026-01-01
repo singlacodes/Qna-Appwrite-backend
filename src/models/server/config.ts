99d5d7cef304edace9da42571a5344834d0488f1
singlacodes <shivamsinglatechnical@gmail.com>
2026-01-01 21:45:21 +0530

    added src/models/name.ts

--- src/models/server/config.ts
+++ src/models/server/config.ts
@@ -12,7 +12,7 @@ client
 const databases = new Databases(client)
 const avatars = new Avatars(client);
 const storage = new Storage(client);
-
+const users = new Users(client)
 
 
 export { client, databases, users, avatars, storage}
